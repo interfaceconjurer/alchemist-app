5072fbf6e191a8aeff828799c6fabc4af4dd04fd
_| Ø |? |] Å |\| <j.wright@salesforce.com>
2018-06-09 22:19:34 -0700

    initial app set up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
-import logo from './graphix/alchemy-03.svg';
+import symbols from './symbols/symbols';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      counter:0
+    }
+  }
+
+  handleTransitionEffect = (event) => {
+    const element = event.target;
+    element.classList.remove('animating');
+    this.updateCounter();
+    element.src = symbols[this.state.counter];
+    setTimeout(() => {
+      element.classList.add('animating');
+    },50)
+    
+  }
+
+  updateCounter = () => {
+    let newCounter = symbols[this.state.counter + 1] ? this.state.counter + 1 : 0;
+    
+    this.setState((prevState) => ({
+      counter: newCounter
+    }));
+  }
+  
+  componentDidMount(){
+    const symbol = this.refs.symbol;
+    symbol.classList.add('animating');
+    symbol.addEventListener('animationend', this.handleTransitionEffect);
+  }
+
   render() {
-    console.log(logo);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={symbols[this.state.counter]} ref='symbol' className="symbol" alt="alchemy symbol" />
+          <h1 className="App-title">Jordan L. Wright</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Product Strategist, Designer &amp; UXE
         </p>
       </div>
     );
+    
   }
 }
 
