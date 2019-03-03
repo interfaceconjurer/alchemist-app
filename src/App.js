579bdfce36176e1010d6337593ebdf9c2dc397af
Jordan Wright <j.wright@salesforce.com>
2019-03-02 23:58:33 -0800

    case changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Route, Switch} from "react-router-dom";
 import ReactGA from 'react-ga';
 import './App.css';
-// import Icon from './components/icon/view';
-// import { icons } from './components/iconList/iconList';
+import Icon from './components/icon/view';
+import { icons } from './components/iconList/iconList';
 import AlchemySymbol from './components/alchemySymbol/mediator';
 import WorkLayout from './components/workLayout/mediator';
 import Modal from './components/modal/mediator';
@@ -60,11 +60,11 @@ class App extends Component {
           <nav className='nav'>
             <a className="out-bound-link" target="_blank" rel="noopener noreferrer" href="http://www.linkedin.com/in/jordan-l-wright-91b17321">
               <span>LinkedIn</span>
-              {/* <Icon title="Out-Bound Link" icon={icons.outboundlink} assistiveText="Out-Bound Link"/> */}
+              <Icon title="Out-Bound Link" icon={icons.outboundlink} assistiveText="Out-Bound Link"/>
             </a>
             <a className="out-bound-link" target="_blank" rel="noopener noreferrer" href="http://digitalchem.ist/resume/Jordan_L_Wright.pdf">
               <span>Resume</span>
-              {/* <Icon title="Out-Bound Link" icon={icons.outboundlink} assistiveText="Out-Bound Link"/> */}
+              <Icon title="Out-Bound Link" icon={icons.outboundlink} assistiveText="Out-Bound Link"/>
             </a>
           </nav>
           <div className='divider'></div>
