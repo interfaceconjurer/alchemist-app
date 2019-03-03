c56cab0ef31ce7f362d7b46337dd7f86147e218c
Jordan Wright <j.wright@jwright-ltm6.internal.salesforce.com>
2019-03-02 20:49:36 -0800

    resume path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,7 +8,10 @@ import registerServiceWorker from './registerServiceWorker';
 const Root = () => {
   return (
     <Router>
-      <Route path="/" component={App} />
+      <Switch>
+        <Route path="/" component={App} />
+        <Route path='/resume' exact component={() => { window.location = 'http://digitalchem.ist/resume/Jordan_L_Wright.pdf'; return null;} }/>
+      </Switch>
     </Router>
   );
 }
