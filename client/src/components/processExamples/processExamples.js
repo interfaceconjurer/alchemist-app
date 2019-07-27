7429a4fb0c5475922acab276430d7ce890689f06
Jordan L. Wright <adigitalchemist@gmail.com>
2019-07-27 11:53:36 -0700

    workitem clean up animation function, added hover and focus for process work caption interaction

--- client/src/components/processExamples/processExamples.js
+++ client/src/components/processExamples/processExamples.js
@@ -7,12 +7,16 @@ import DynamicForms from '../../graphix/Dynamic-Forms.png';
 
 
 class ProcessExamples extends Component {
-  handleFocus = (event) => {
-    event.target.nextElementSibling.classList.add('pwc-show');
+  handleInput = (event) => {
+    let needsFocus = event.type === 'focus' || event.type === 'mouseover';
+    let needsBlur = event.type === 'blur' || event.type === 'mouseout';
+    if(needsFocus === true){
+      event.target.nextElementSibling.classList.add('pwc-show');
+    }else if(needsBlur === true){
+      event.target.nextElementSibling.classList.remove('pwc-show');
+    }
   }
-  handleBlur = (event) => {
-    event.target.nextElementSibling.classList.remove('pwc-show');
-  }
+  
   render(){
     return(
       <section className="process-examples-wrap">
@@ -20,7 +24,13 @@ class ProcessExamples extends Component {
         <div className="process-examples">
           <ProcessWork>
             <figure className='process-work-image-holder'>
-              <img onFocus={this.handleFocus} onBlur={this.handleBlur} tabIndex="0" src={VisionSprint} alt="Mortgage Lending Vision Sprint"/>
+              <img 
+                onFocus={this.handleInput} 
+                onBlur={this.handleInput} 
+                onMouseOver={this.handleInput} 
+                onMouseOut={this.handleInput} 
+                tabIndex="0" src={VisionSprint} 
+                alt="Mortgage Lending Vision Sprint"/>
               <figcaption className='process-work-caption'>Available Upon Request</figcaption>
             </figure>
             <p>This Vision Sprint was dedicated to mapping out a 5 year vision for Salesforce to build out a comprehensive solution for the Mortgage Industry. </p>
@@ -28,7 +38,13 @@ class ProcessExamples extends Component {
           </ ProcessWork>
           <ProcessWork>
             <figure className='process-work-image-holder'>
-              <img onFocus={this.handleFocus} onBlur={this.handleBlur} tabIndex="0" src={Mango} alt="Project Mango"/>
+              <img 
+                onFocus={this.handleInput} 
+                onBlur={this.handleInput} 
+                onMouseOver={this.handleInput} 
+                onMouseOut={this.handleInput} 
+                tabIndex="0" src={Mango} 
+                alt="Project Mango"/>
               <figcaption className='process-work-caption'>Available Upon Request</figcaption>
             </figure>
             <p>Project Mango is an idea around document collection, verification, approval and providing transparency in the mortgage process. </p>
@@ -36,7 +52,14 @@ class ProcessExamples extends Component {
           </ ProcessWork>
           <ProcessWork>
             <figure className='process-work-image-holder'>
-              <img onFocus={this.handleFocus} onBlur={this.handleBlur} tabIndex="0" src={DynamicForms} alt="Mortgage Lending Vision Sprint"/>
+              <img 
+                onFocus={this.handleInput} 
+                onBlur={this.handleInput} 
+                onMouseOver={this.handleInput} 
+                onMouseOut={this.handleInput}
+                tabIndex="0" 
+                src={DynamicForms} 
+                alt="Mortgage Lending Vision Sprint"/>
               <figcaption className='process-work-caption'>Available Upon Request</figcaption>
             </figure>
             <p>Dynamic Forms is work I did around digitizing the Residential Loan Application in Mortgage on the Salesforce Platform.</p>
