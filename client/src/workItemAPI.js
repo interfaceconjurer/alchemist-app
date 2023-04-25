a4f43105c970d2e492be87fb5dc625a4cf3ca681
Jordan L Wright <47576172+interfaceconjurer@users.noreply.github.com>
2023-04-25 00:51:18 -0400

    fix dribble ref

--- client/src/workItemAPI.js
+++ client/src/workItemAPI.js
@@ -5,7 +5,7 @@ export const workItemAPI = {
     if(this.workItemData.length === 0){
       const key = process.env.REACT_APP_DRIBBBLE_TOKEN
       const url = 'https://api.dribbble.com/v2/user/shots?page=1&per_page=100';
-      const response = await fetch(url + '&access_token=' + "5cecd738e40e30f52442e8753d4cd5f05148782252c87c63c62101f24fd3d6fa");
+      const response = await fetch(url + '&access_token=' + "key");
       data = await response.json();
       this.workItemData = data;
     } else{
