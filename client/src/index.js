b713e99ef6d1f84d00ea02cde293eb697944b56e
Jordan L Wright <47576172+interfaceconjurer@users.noreply.github.com>
2023-12-14 15:18:59 -0500

    remove service worker

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const Root = () => {
   return (
